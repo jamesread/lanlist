40f69b96782169f00ff8342fbb4f3f8e80c5791e
jamesread <contact@jwread.com>
2015-09-16 19:05:31 +0100

    Title on the viewUser page

--- viewUser.php
+++ viewUser.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once 'includes/widgets/header.php';
+require_once 'includes/common.php';
 
 requirePriv('USERLIST');
 
@@ -12,21 +12,23 @@ $stmt->execute();
 
 if ($stmt->numRows() == 0) {
 	throw new Exception('user not found');
-} else {
-	$user = $stmt->fetchRow();
-
-	echo '<h2>User: ' . $user['username'] . '</h2>';
-	echo 'Steam username: ' . (empty($user['usernameSteam']) ? '???' : $user['usernameSteam']) . '<br />';
-	echo 'Primary group: ' . $user['groupTitle'] . '<br />';
-	echo 'Last login: ' . $user['lastLogin'] . '<br />';
-	echo 'Registered: ' . issetor($user['registered']) . '<br />';
-	echo 'Email: ' . issetor($user['email']) . '<br />';
-
-	if (!empty($user['organizerId'])) {
-		echo 'Organizer: <a href = "viewOrganizer.php?id=' . $user['organizerId'] . '">' . $user['organizerTitle'] . '</a>';
-	}
 }
 
+$user = $stmt->fetchRow();
+
+define('TITLE', 'User: ' . $user['username']);
+require_once 'includes/widgets/header.php';
+
+echo '<h2>User: ' . $user['username'] . '</h2>';
+echo 'Steam username: ' . (empty($user['usernameSteam']) ? '???' : $user['usernameSteam']) . '<br />';
+echo 'Primary group: ' . $user['groupTitle'] . '<br />';
+echo 'Last login: ' . $user['lastLogin'] . '<br />';
+echo 'Registered: ' . issetor($user['registered']) . '<br />';
+echo 'Email: ' . issetor($user['email']) . '<br />';
+
+if (!empty($user['organizerId'])) {
+	echo 'Organizer: <a href = "viewOrganizer.php?id=' . $user['organizerId'] . '">' . $user['organizerTitle'] . '</a>';
+}
 
 if (Session::getUser()->hasPriv('USER_EMAIL_LOG')) {
 	$sql = 'SELECT l.id, l.sent, l.subject FROM email_log l WHERE l.emailAddress = :emailAddress ORDER BY l.sent DESC LIMIT 10';
