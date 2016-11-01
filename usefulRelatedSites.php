68c8b80961075be675c8ab46dfdf3ae6d7fd27b9
jamesread <contact@jwread.com>
2016-11-01 12:34:49 +0000

    Remove some old sites.

--- usefulRelatedSites.php
+++ usefulRelatedSites.php
@@ -4,10 +4,7 @@ require_once 'includes/widgets/header.php';
 
 ?>
 	<h2>Useful related sites</h2>
-	<p>You may find these links helpful...</p>
-	<ul>
-		<li><a href = "http://www.network-games-collection.com/" target = "_new">Network Games Collection</a> - A database of games that support LAN play. </li>
-		<li><a href = "http://www.lanpartywiki.com/wiki/Main_Page" target = "_new">LANPartyWiki</a> - A wikipedia-like wiki for all things related to LAN Parties.</li>
+	<p>We used to have some useful links here, but those sites died out :(</p>
 
 <?php
 
