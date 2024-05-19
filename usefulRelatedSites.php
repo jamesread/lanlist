dcc8c5b9698851e2ef01180ea502cd2f4cf5bfc7
jamesread <contact@jread.com>
2024-05-19 21:34:21 +0100

    Start restoring useful links

--- usefulRelatedSites.php
+++ usefulRelatedSites.php
@@ -3,8 +3,12 @@
 require_once 'includes/widgets/header.php';
 
 ?>
-	<h2>Useful related sites</h2>
-	<p>We used to have some useful links here, but those sites died out :(</p>
+        <h2>Useful related sites</h2>
+<ul>
+    <li><a href = "https://lan.party">lan.party</a> - A site created in April 2024, hosting resources for the LAN party community.</li>
+    <li><a href = "https://lanparty.dk">lanparty.dk</a> - A community and event list for LAN parties in and near Denmark.</li>
+    <li><a href = "http://www.lanpartywiki.net" target = "_new">LANPartyWiki</a> - A wikipedia-like wiki for all things related to LAN Parties.</li>
+</ul>
 
 <?php
 
