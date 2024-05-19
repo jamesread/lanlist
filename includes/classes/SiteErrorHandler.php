fb1e679daeeb4b48a0053c0d6b7ced8b3f7fde64
jamesread <contact@jread.com>
2024-05-19 01:58:47 +0100

    Improve error message friendliness, and dark mode

--- includes/classes/SiteErrorHandler.php
+++ includes/classes/SiteErrorHandler.php
@@ -1,7 +1,7 @@
 <?php
 
 class SiteErrorHandler extends \libAllure\ErrorHandler {
-	protected function renderSfe(\libAllure\exceptions\SimpleFatalError $e) {
+	public function renderSfe($e) {
 		require_once 'includes/widgets/header.php';
 		echo '<p><span class = "karmaBad">Error: ' . $e->getMessage() . '</span></p>';
 		echo '<p>This type of error is probably caused by something that you did. Try going back in your browser and try again.</p>';
